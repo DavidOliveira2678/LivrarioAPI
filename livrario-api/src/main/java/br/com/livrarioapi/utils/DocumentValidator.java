ffac8b9e565aad02d0cef50882eb9d18b568f63b
DavidOliveira2678 <david.willyam23@gmail.com>
2026-08-18 15:58:08 -0300

    feat(utils): adiciona a validação de ISBNs no validador de documentos

--- livrario-api/src/main/java/br/com/livrarioapi/utils/DocumentValidator.java
+++ livrario-api/src/main/java/br/com/livrarioapi/utils/DocumentValidator.java
@@ -1,12 +1,17 @@
 package br.com.livrarioapi.utils;
 
-import br.com.caelum.stella.ValidationMessage;
 import br.com.caelum.stella.validation.CPFValidator;
 import br.com.caelum.stella.validation.InvalidStateException;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import org.hibernate.validator.constraints.ISBN;
 
-import java.util.List;
+import java.util.Set;
 
 public class DocumentValidator {
+    private static final Validator VALIDATOR = Validation.buildDefaultValidatorFactory().getValidator();
+
     public static boolean isCpfValido(String cpf){
         try{
             CPFValidator validator = new CPFValidator();
@@ -18,4 +23,18 @@ public class DocumentValidator {
         }
     }
 
+    public static boolean isIsbnValido(String isbn){
+        Set<ConstraintViolation<IsbnHolder>> violations = VALIDATOR.validate(new IsbnHolder(isbn));
+
+        return violations.isEmpty();
+    }
+
+    private static class IsbnHolder{
+        @ISBN
+        String value;
+
+        IsbnHolder(String value){
+            this.value = value;
+        }
+    }
 }
